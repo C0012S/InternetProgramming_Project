495676e86110b6e4a5545665e5a1d36018484505
C0012S <qkrtkd1002@duksung.ac.kr>
2021-12-13 03:32:33 +0900

    CBV로 쇼핑몰 페이지 만들기

--- mall/urls.py
+++ mall/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ # 서버IP/mall/
-    path('', views.index),
-    path('<int:pk>/', views.single_item_page),
+#    path('', views.index),
+#    path('<int:pk>/', views.single_item_page),
+
+    path('', views.ItemList.as_view()),
+    path('<int:pk>/', views.ItemDetail.as_view()),
 ]
--- mall/views.py
+++ mall/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from . models import Item
 
 # Create your views here.
-def index(request):
-    items = Item.objects.all().order_by('-pk')
+class ItemList(ListView) :
+    model = Item
+    ordering = '-pk'
+#    template_name = 'mall/index.html'
+# item_list.html
 
-    return render(request, 'mall/index.html',
-                  {
-                      'items' : items
-                  }
-                  )
+class ItemDetail(DetailView) :
+    model = Item
+# item_detail.html
 
-def single_item_page(request, pk):
-    item = Item.objects.get(pk=pk)
-
-    return render(request, 'mall/single_item_page.html',
-                  {
-                      'item' : item
-                  }
-                  )
+# def index(request):
+#     items = Item.objects.all().order_by('-pk')
+#
+#     return render(request, 'mall/index.html',
+#                   {
+#                       'items' : items
+#                   }
+#                   )
+#
+# def single_item_page(request, pk):
+#     item = Item.objects.get(pk=pk)
+#
+#     return render(request, 'mall/single_item_page.html',
+#                   {
+#                       'item' : item
+#                   }
+#                   )
