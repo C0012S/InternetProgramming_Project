e638b57b8363aeb6ca1b4f237d96e9526adee1ec
C0012S <qkrtkd1002@duksung.ac.kr>
2021-12-16 00:25:33 +0900

    author 추가하기

--- mall/models.py
+++ mall/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -27,8 +28,10 @@ class Item(models.Model):
 #    maker = models.ForeignKey(Maker, null=True, blank=True, on_delete=models.SET_NULL) # 제조사 포함 (다대일 관계)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL) # 카테고리 포함 (다대일 관계)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
-        return f'[{self.pk}]{self.item_name}'
+        return f'[{self.pk}]{self.item_name} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/mall/{self.pk}'
