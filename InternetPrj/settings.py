5ecb971dddf0977eb0c59e8a879b4b05c2e4499d
C0012S <qkrtkd1002@duksung.ac.kr>
2021-12-16 05:08:37 +0900

    상품 등록/수정 페이지에 crispy-forms 라이브러리 이용하기

--- InternetPrj/settings.py
+++ InternetPrj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'mall',
     'single_pages',
 ]
@@ -118,6 +119,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
