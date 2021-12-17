2ce83156ace550787d54ddfea75ded495ef6b81f
C0012S <qkrtkd1002@duksung.ac.kr>
2021-12-18 01:48:20 +0900

    홈 페이지 수정하기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from mall.models import Item
 
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'single_pages/home_page.html')
+    recent_items = Item.objects.order_by('-pk')[:3]
+    return render(request, 'single_pages/home_page.html',
+                  {'recent_items' : recent_items})
 
 def my_page(request):
     return render(request, 'single_pages/my_page.html')
